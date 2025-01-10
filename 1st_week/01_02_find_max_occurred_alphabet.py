32bf0abde879cd074a4fc30643422256c2e1f8e8
yuseokkim <last1224@gmail.com>
2025-01-10 13:52:31 +0900

    최빈값을 구하는데 사용되는 파이썬 코드의 주석을 추가

--- 1st_week/01_02_find_max_occurred_alphabet.py
+++ 1st_week/01_02_find_max_occurred_alphabet.py
@@ -1,4 +1,10 @@
 # alphabet 총 길이 26
+# ord(문자): 문자를 아스키 코드로 변환
+# chr(아스키코드): 아스키코드를 문자로 변환
+# range(number): 정수의 길이만큼 순회
+# len(list): 리스트의 길이를 가져옴
+# for i in range(len(list)): 리스트의 길이만큼 순회
+# enumerate(list): 리스트를 인덱스와 값을 가진 객체로 변환 -> for i, v in enumerate(list)
 
 def find_max_occurred_alphabet(string):
     alphabet_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
